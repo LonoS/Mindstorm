ede804ca5289d72190f09d6b0855eb4f94afe8e4
LonoS <lonosgames@gmail.com>
2018-06-08 09:16:58 +0200

    added variables class

---
+++ src/krennKronlachner/mindstorm/main/variablesClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package krennKronlachner.mindstorm.main;
+
+/**
+ *
+ * @author Kroni
+ */
+public class variablesClass {
+    public static boolean driveForward = true;
+    public static boolean driveLeft = false;
+    public static boolean driveRight = false;
+    public static boolean stop = false;
+    public static boolean driveBackwards = false; 
+    
+}
